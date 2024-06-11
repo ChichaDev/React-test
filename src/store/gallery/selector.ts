e1722759cab360f04b73399fd77d19be53416042
Vladislav Polizhay <polizhaiofficial@gmail.com>
2024-06-11 14:13:59 +0300

    feat(react-test): optimization reselect

--- src/store/gallery/selector.ts
+++ src/store/gallery/selector.ts
@@ -1,6 +1,10 @@
+import { createSelector } from 'reselect';
+
 import type { RootState } from '../store';
 
-export const getGallery = (state: RootState) => state.gallery.galleryList;
+const galleryState = (state: RootState) => state.gallery;
+
+export const getGallery = createSelector(galleryState, (gallery) => gallery.galleryList);
 
 export const getStatusGallery = (state: RootState) => state.gallery.status;
 
